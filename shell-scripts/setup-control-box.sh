1c6c48e8835bc655e6e0a944866a1d211586146c
Otavio Medeiros <otaviorm@gmail.com>
2016-05-09 18:37:03 -0300

    provisioning control machine with ansible

--- shell-scripts/setup-control-box.sh
+++ shell-scripts/setup-control-box.sh
@@ -5,3 +5,8 @@ fi
 cp /home/vagrant/.ssh/id_rsa.pub /vagrant/control.pub
 
 chown -R vagrant:vagrant /home/vagrant/.ssh/
+
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get -y update
+sudo apt-get -y install ansible
